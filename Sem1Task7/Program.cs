93ee22da5b356cade1aeba3bebda15204f0604f8
Petr <gram07@list.ru>
2022-12-01 18:58:09 +0300

    Sem1 Task 7 added

---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,14 @@
+﻿//Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустыми
+if (inputLine != null)
+{
+
+    //Парсим введённое число
+    int inputNumber = int.Parse(inputLine);
+
+    int lastDigit = inputNumber % 10;
+
+    Console.WriteLine(lastDigit);
+}
