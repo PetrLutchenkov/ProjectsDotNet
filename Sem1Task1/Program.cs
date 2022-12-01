fb51ff1f2737952309ed4683223bdb205da237f9
Petr <gram07@list.ru>
2022-12-01 17:40:09 +0300

    Sem1 Task 1 added

--- Sem1Task1/Program.cs
+++ Sem1Task1/Program.cs
@@ -1,18 +1,21 @@
 ﻿
 //Считываем данные с консоли
-string? Num1Line = Console.ReadLine();
-string? Num2Line = Console.ReadLine();
+string? num1Line = Console.ReadLine();
+string? num2Line = Console.ReadLine();
 
 //Проверяем, чтобы данные были не пустыми
-if(Num1Line != null && Num2Line != null)
+if (num1Line != null && num2Line != null)
 {
     //Парсим (переводим в целочисленные значения) введённые числа
-    int num1 = int.Parse(Num1Line);
-    int num2 = int.Parse(Num2Line);
+    int num1 = int.Parse(num1Line);
+    int num2 = int.Parse(num2Line);
 
-    //Находит произведение
-    int outResult = num1*num2;
-
-    //Выводим данные в консоль
-    Console.WriteLine(outResult);
+   if (num1 == num2 * num2)
+   {
+    Console.WriteLine("Первое число квадрат второго!");
+   }
+   else
+   {
+    Console.WriteLine("Первое число не квадрат второго!");
+   }
 }
