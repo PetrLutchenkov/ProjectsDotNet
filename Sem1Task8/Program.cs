a709fa1016bfd265d7bd9eb9d11f0d3f36ddd3cb
Petr <gram07@list.ru>
2022-12-01 20:38:19 +0300

    Sem1 Task 8 added

---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,28 @@
+﻿//Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустыми
+if (inputLine != null)
+{
+    //Парсим введённое число
+    int inputNumber = int.Parse(inputLine);
+
+    //Число для старта цикла
+    int startNumber = 2;
+
+    //Выходное значение
+    string outLine = string.Empty;
+
+    //Делаем цикл, добавляем в строку вывода только чётные числа
+    while (startNumber < inputNumber)
+    {
+        outLine = outLine + startNumber + ", ";
+        startNumber = startNumber + 2;
+    }
+    //Проверяем чётное ли введённое число
+    if (inputNumber % 2 == 0)
+        outLine = outLine + inputNumber;
+
+    //Выводим данные в консоль
+    Console.WriteLine(outLine);
+}
