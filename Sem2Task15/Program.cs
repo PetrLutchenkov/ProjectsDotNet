230e46a2b7b9ab5d707ccff75170f2796e9686ac
Petr <gram07@list.ru>
2022-12-05 19:37:08 +0300

    Sem2 Task 15 added

---
+++ Sem2Task15/Program.cs
@@ -0,0 +1,22 @@
+﻿//Считываем данные с консоли
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+//Проверяем число на соответствие дню недели
+if (number < 8 && number != 0)
+{
+    if (number > 5)
+    {
+        //Выводим данные в консоль
+        Console.WriteLine("да");
+    }
+    else
+    {
+        //Выводим данные в консоль
+        Console.WriteLine("нет");
+    }
+}
+else
+{
+    //Выводим данные в консоль
+    Console.WriteLine("Цифра не соответствует дню недели");
+}
