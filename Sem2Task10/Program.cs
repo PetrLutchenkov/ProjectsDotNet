fc912cf7bd159e343003f441fcc6e65cc8ec0528
Petr <gram07@list.ru>
2022-12-05 19:09:12 +0300

    Sem2 Task 10 added

---
+++ Sem2Task10/Program.cs
@@ -0,0 +1,17 @@
+﻿//Считываем данные с консоли
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+//Проверяем,что число трёхзначное
+if (number > 99 && number < 1000)
+{
+    //Получаем второе число
+    int secondSign = number / 10 % 10;
+
+    //Выводим данные в консоль
+    Console.WriteLine(secondSign);
+}
+else
+{
+    //Выводим данные в консоль
+    Console.WriteLine("Введите трёхзначное число!");
+}
