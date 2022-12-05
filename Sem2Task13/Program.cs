d0c55b9587cc0b4132af4a755ce31d48ab4bdb96
Petr <gram07@list.ru>
2022-12-05 19:25:00 +0300

    Sem2 Task 13 added

---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,17 @@
+﻿//Считываем данные с консоли
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+//Проверяем имеет ли число третью цифру
+if (number > 99)
+{
+    //Превращаем число в массив
+    Char[] digits = number.ToString().ToCharArray();
+
+    //Выводим третий элемент массива
+    Console.WriteLine(digits[2]);
+}
+else
+{
+    //Выводим данные в консоль
+    Console.WriteLine("Третьей цифры нет");
+}
