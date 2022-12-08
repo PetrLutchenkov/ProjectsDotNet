45c4ace2575f1ccd5511c0277944def7649b9d57
Petr <gram07@list.ru>
2022-12-08 14:49:57 +0300

    Sem3 Tasks 17, 18, 20, 22 added

---
+++ Sem3Task20/Program.cs
@@ -0,0 +1,26 @@
+﻿int X1 = ReadData("Введите координату Х1: ");
+int Y1 = ReadData("Введите координату Y1: ");
+int X2 = ReadData("Введите координату Х2: ");
+int Y2 = ReadData("Введите координату Y2: ");
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод находит расстояние между точками на плоскости
+double DistanceBetweenPoints(double x1, double y1, double x2, double y2)
+{
+    double distance = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    return distance;
+}
+
+//Метод выводит данные пользователю
+void PrintData(string msg, double res)
+{
+    Console.WriteLine(msg + res);
+}
+
+PrintData("Расстояние между точками: ", DistanceBetweenPoints(X1, Y1, X2, Y2));
